d6022b1ee4e9e472ae9deb838d61cfc8f41ef6d4
jimvanveenhuyzen <125078335+jimvanveenhuyzen@users.noreply.github.com>
2023-03-08 21:59:11 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Download Vandermonde.txt"
+if [ ! -e Vandermonde.txt ]; then
+  wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/Vandermonde.txt 
+fi
+
 python3 NURhandin1_1a 
-python3 NURhandin1_2a
+python3 NURhandin1_2
 
 echo "Generating the pdf"
 
