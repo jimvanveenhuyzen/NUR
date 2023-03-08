0d0d37ca0fae49f8051068360209df2946f446a9
jimvanveenhuyzen <125078335+jimvanveenhuyzen@users.noreply.github.com>
2023-03-08 23:30:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Download Vandermonde.txt"
+echo "Download Vandermonde.txt if not present so you're able to run test NURhandin1_2"
 if [ ! -e Vandermonde.txt ]; then
   wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/Vandermonde.txt 
 fi
